54ab7f4abb349cfc9c6ddf51e627d4e4ff4b4e64
d4rkr4in <110217352+d4rkr4in@users.noreply.github.com>
2025-08-12 13:38:28 +0300

    Update check_hiddify.sh

--- check_hiddify.sh
+++ check_hiddify.sh
@@ -5,9 +5,11 @@ IP=$(curl --interface tun0 -s --max-time 10 ifconfig.me)
 
 # If curl fails or returns empty, restart services
 if [ -z "$IP" ]; then
+  echo "No response via tun0. Restarting HiddifyCli and Tun2Socks..."
   logger -t check_hiddify "No response via tun0. Restarting HiddifyCli and Tun2Socks..."
   /etc/init.d/HiddifyCli restart
   /etc/init.d/tun2socks restart
 else
+  echo "tun0 is working. IP: $IP"
   logger -t check_hiddify "tun0 is working. IP: $IP"
 fi
