a43f97b225a41cbd7820cd1efbb40ccf67cf8eb4
d4rkr4in <110217352+d4rkr4in@users.noreply.github.com>
2025-08-13 15:32:00 +0300

    Update check_pbr.sh

--- check_pbr.sh
+++ check_pbr.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 # Название сервиса в OpenWrt
 SERVICE="pbr"
+TAG="check_pbr"
 
 # Формат времени
 TIMESTAMP="$(date '+%Y-%m-%d %H:%M:%S')"
@@ -7,11 +10,11 @@ TIMESTAMP="$(date '+%Y-%m-%d %H:%M:%S')"
 # Проверка состояния
 if ! /etc/init.d/$SERVICE status >/dev/null 2>&1; then
     MESSAGE="$TIMESTAMP — сервис $SERVICE не запущен. Запускаю..."
-    echo "$MESSAGE"
-    logger -t "$SERVICE-check" "$MESSAGE"
     /etc/init.d/$SERVICE start
 else
     MESSAGE="$TIMESTAMP — сервис $SERVICE работает."
-    echo "$MESSAGE"
-    logger -t "$SERVICE-check" "$MESSAGE"
 fi
+
+# Вывод и логирование
+echo "$MESSAGE"
+logger -t "$TAG" "$MESSAGE"
