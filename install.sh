e79c887eca3a407ba7822b3a9ee9186d004d13b5
d4rkr4in <110217352+d4rkr4in@users.noreply.github.com>
2025-09-19 08:54:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ fi
 # Устанавливаем нужные пакеты
 opkg install curl nano luci-theme-openwrt-2020
 
-# Отключаем ipv6 через скрипт ipv6_off.sh
-wget -O ipv6_off.sh https://raw.githubusercontent.com/d4rkr4in/hiddify_openwrt/refs/heads/main/ipv6_off.sh
-chmod +x ipv6_off.sh
-./ipv6_off.sh
-
 # Цикл запроса ссылки на подписку, пока она пуста
 while true; do
   read -p "Введите ССЫЛКУ_НА_ПОДПИСКУ: " SUBSCRIPTION_LINK
