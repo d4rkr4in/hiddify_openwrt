2aedf2c6986839c3abb9f63e3b1d0605de5e167e
d4rkr4in <110217352+d4rkr4in@users.noreply.github.com>
2026-02-12 09:22:26 +0300

    Update get_cidr4.sh

--- get_cidr4.sh
+++ get_cidr4.sh
@@ -1,1 +1,17 @@
-wget -O cidr4.txt "https://iplist.opencck.org/?format=comma&data=cidr4" && sed -i 's/,//g' cidr4.txt
+TMP_FILE=$(mktemp)
+
+if wget -q -O "$TMP_FILE" "https://iplist.opencck.org/?format=comma&data=cidr4"; then
+    sed -i 's/,//g' "$TMP_FILE"
+
+    if ! cmp -s "$TMP_FILE" cidr4.txt; then
+        mv "$TMP_FILE" cidr4.txt
+        echo "Файл обновлён"
+    else
+        rm "$TMP_FILE"
+        echo "Изменений нет"
+    fi
+else
+    echo "Ошибка загрузки"
+    rm -f "$TMP_FILE"
+    exit 1
+fi
